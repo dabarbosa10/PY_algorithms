7d8045831ee9bbefabfe8413438ff8356041d4e7
Diego Andrés Barbosa Trujillo <da.barbosa10@uniandes.edu.co>
2024-03-17 16:29:32 -0500

    Updating classes

--- nextL.py
+++ nextL.py
@@ -1,1 +1,11 @@
 # Node stores data and a reference to the next node.
+class Node:
+    def __init__(self,data, next=None):
+        self.data=data # Data stored in the node
+        self.next=next
+
+class Linked:
+    def __init__(self,head=None):
+        self.head=head
+        
+    
